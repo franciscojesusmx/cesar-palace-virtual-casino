fb67fdcabbfcec986facf08fb1f873e7da67e909
Carlos Urias <hicarlosurias@gmail.com>
2023-08-09 18:28:10 -0700

    Implement player and dealer card display with correct crupier behavior

--- app/blackjack.py
+++ app/blackjack.py
@@ -10,7 +10,7 @@ def create_card():
             card.append((suit, rank))
     return card
 
-def get_hand(cards, size_hand):
+def get_hand(cards, size_hand=2):
     hand = random.sample(cards, size_hand )
     return hand
 
@@ -27,7 +27,7 @@ def calculate_hand_sum(hand):
     total = sum(card_value(card) for card in hand)
     # Adjust the value of Ace if needed (to avoid exceeding 21)
     for card in hand:
-        if card[1] == 'ace' and total > 21:
+        if card[1] == 'as' and total > 21:
             total -= 10
     return total
 
@@ -37,3 +37,28 @@ def crupier_hand(cards):
         hand.append(random.choice(cards))
     return hand
 
+def blackjack():
+    cards = create_card()
+
+    player_cards = get_hand(cards)
+    crupier_cards = crupier_hand(cards)
+
+    print("Your initial two cards:")
+    for card in player_cards:
+        print(f"{card[1]} of {card[0]}")
+    print(f"Sum: {calculate_hand_sum(player_cards)}\n")
+
+    print("Crupier's face-up card:")
+    print(f"{crupier_cards[0][1]} of {crupier_cards[0][0]}\n")
+
+    # Crupier's turn
+    while calculate_hand_sum(crupier_cards) < 17:
+        new_card = random.choice(cards)
+        crupier_cards.append(new_card)
+
+    print("Crupier's final hand:")
+    for card in crupier_cards:
+        print(f"{card[1]} of {card[0]}")
+    print(f"Sum: {calculate_hand_sum(crupier_cards)}\n")
+
+blackjack()
