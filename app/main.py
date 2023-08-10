888ef940c8d7534065f3ab80111eb11aa07cafc4
Carlos Urias <hicarlosurias@gmail.com>
2023-08-10 08:17:57 -0700

    Requirements and main menu to play

--- app/main.py
+++ app/main.py
@@ -2,4 +2,17 @@ from blackjack import blackjack
 
 def menu():
     print("*** WELCOME TO CESAR PALACE CASINO ***")
-    print()
+    print("¿What do you wanna play now?")
+    print("1. - Blackjack")
+    
+
+    choice = input("Choice an option -> ")
+    return choice
+
+while True:
+    choice = menu()
+
+    if choice == "1":
+        blackjack()
+    else:
+        print("Select a valid option")
